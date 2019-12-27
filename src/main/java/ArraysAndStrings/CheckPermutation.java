d830a21a42f64a61a973a469e9370bc663c02b63
elrub <rubinovaea@gmail.com>
2019-12-26 20:49:01 -0800

    1.2 isPermutation using array

--- src/main/java/ArraysAndStrings/CheckPermutation.java
+++ src/main/java/ArraysAndStrings/CheckPermutation.java
@@ -1,6 +1,9 @@
 package ArraysAndStrings;
 
 // given two strings, decide if one is a permutation of another
+// 1) use .sort() and .equals()
+// 2) use hash set
+// 3) use array like a hash set to map character occurrences
 
 // are strings case-sensitive?
 // do white spaces count?
@@ -10,11 +13,10 @@ import java.util.HashSet;
 public class CheckPermutation {
 
     // method 1: hash set
-    public static boolean isPermutation(String s1, String s2) {
+    public static boolean isPermutation1(String s1, String s2) {
         if (s1.length() != s2.length()) {
             return false;
         }
-
         HashSet<Character> chars = new HashSet<Character>();
 
         // add first word to a hash set
@@ -32,10 +34,30 @@ public class CheckPermutation {
         if (chars.isEmpty()) {
             return true;
         }
-
         return false;
     }
 
-    // method 2: sort and compare
+    // method 2: using array
+    public static boolean isPermutation2(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        int[] chars = new int[128];
+
+        // map characters of the first string by incrementing their indexes in array
+        for (int i = 0; i < s1.length(); i++) {
+            chars[s1.charAt(i)]++;
+        }
+
+        // decrement indexes of s2 characters. If any of them becomes -1, return false
+        for (int i = 0; i < s2.length(); i++) {
+            chars[s2.charAt(i)]--;
+            if (chars[s2.charAt(i)] < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
